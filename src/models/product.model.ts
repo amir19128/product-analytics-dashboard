8268a18ffbc26f0300d42f61f353ca87d3c06968
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2026-02-13 01:09:16 +0330

    feat: add Product interface for product data

---
+++ src/models/product.model.ts
@@ -0,0 +1,15 @@
+// src/models/product.model.ts
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  thumbnail: string;
+}
+
+export interface ProductsResult {
+  products: Product[];
+  total: number;
+}
